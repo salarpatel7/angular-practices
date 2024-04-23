eb5cfb2e51baf9b1ce05173406aa0d444e3d3e2e
salarpatel7 <salarpatel19@gmail.com>
2024-04-23 14:44:43 +0530

    add api in the form

--- angular-home/src/app/components/form/form.component.ts
+++ angular-home/src/app/components/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { FormService } from 'src/app/services/form.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
+  constructor(private formapi:FormService){}
+
+  ngOnInit(){
+    this.fetchingContacts()
+  }
+
+  details:any
+  fetchingContacts(){
+  this.formapi.getFormData().subscribe(
+    (data:any)=>{
+      this.details=data
+      console.log(data);
+    },
+    (error:any)=>{
+      console.log('error getting data',error);
+    }
+  )
+  }
 }
--- angular-home/src/app/services/form.service.ts
+++ angular-home/src/app/services/form.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor() { }
+
+  apiUrl='http://localhost:3000/users'
+  constructor(private http: HttpClient) { }
+
+  getFormData(){
+    return this.http.get(this.apiUrl)
+  }
 }
